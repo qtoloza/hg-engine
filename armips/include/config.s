7afc2c7bf72a175ed7b1132e29b6b7781f4c26c1
qtoloza <20051300+qtoloza@users.noreply.github.com>
2024-12-28 11:47:47 -0500

    disallow save changes for now to let us use pkhex

--- armips/include/config.s
+++ armips/include/config.s
@@ -16,7 +16,7 @@ SNOW_WARNING_GENERATION equ 9
 
 // ALLOW_SAVE_CHANGES tells the assembler that it can build the code that pertains to expanding the save data for extra fields for our usage.
 // this is required for dex expansion and will break PKHeX compatibility!  comment this line out to prevent save changes from being made.
-.definelabel ALLOW_SAVE_CHANGES, 0
+// .definelabel ALLOW_SAVE_CHANGES, 0
 
 // CRY_PSEUDOBANK_START defines the first pseudobank to be used as cries in the sdat.  cries are loaded differently to save on RAM space
 CRY_PSEUDOBANK_START equ 778
